b60c66673b95e3b501359884e6a186d767c5a6a8
TvProTeam <hackersica@gmail.com>
2025-10-04 19:23:33 +0200

    Update _repo_generator.py

--- _repo_generator.py
+++ _repo_generator.py
@@ -1,9 +1,3 @@
-""" 
-    Put this script in the root folder of your repo and it will
-    zip up all addon folders, create a new zip in your zips folder
-    and then update the md5 and addons.xml file
-"""
-
 import hashlib
 import os
 import shutil
