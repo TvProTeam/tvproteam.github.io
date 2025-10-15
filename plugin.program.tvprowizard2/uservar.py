baaca93106f61862aa60505f324a0338237ceab4
TvProTeam <hackersica@gmail.com>
2025-10-15 12:11:15 +0200

    wizard 2

---
+++ plugin.program.tvprowizard2/uservar.py
@@ -0,0 +1,11 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://tvproteam.github.io/repo/wizard/builds-v2.txt'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'http://CHANGEME'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://tvproteam.github.io/repo/wizard/wizard_notify-v2.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
